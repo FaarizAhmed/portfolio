01f5b6c380a753fb8b8038659daa9e4ee841ca99
Monkeyking0001 <gamenft777@gmail.com>
2024-05-16 12:37:07 +0530

    tnd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,7 +95,7 @@ function NavBar() {
 
             {/* <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://frizzfolio.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
